e48553775f8e115d2218ab25a9e0659c5c48ef28
s2414467 <j.li-328@sms.ed.ac.uk>
2024-08-06 12:48:14 +0100

    Added some guidance for gameplay

--- src/components/GameView.vue
+++ src/components/GameView.vue
@@ -2,18 +2,46 @@
   <div class="game-intro">
     <h1>You've mastered the theory, let's test your learning in the game!</h1>
     <h2>Three adventurers are lost in the jungle.
-      Please help them arrange tasks so that they can have dinner as soon as possible.</h2>
+      Please help them arrange tasks so that they can have dinner as soon as possible.<br>
+      Drag the yellow squares representing the tasks to the long bars representing the adventurers for task scheduling!</h2>
     <h3>
       Remember a special rule:<br>
-      when one processor can schedule more than one task,
-      the task with the smallest number is selected for scheduling;<br>
-      when more than one processor can schedule the same task,
-      the task is scheduled by the processor with the smallest number.
+      When an adventurer can handle more than one task, the smallest numbered task will be done first;<br>
+      When more than one adventurer can do a same task, the smallest numbered adventurer handles that task.
     </h3>
   </div>
   <div class="game">
     <iframe :src="gameUrl" style="width: 85vw; height: 90vh;"></iframe>
   </div>
+
+  <div class="game-answer">
+    <button class="toggle-button" @click="toggleContent">
+      {{ isContentVisible ? 'Hide the answer':'Show me the answer' }}
+    </button>
+    <div v-if="isContentVisible">
+      <p>The correct answer is represented in the following format: <br>
+        'Graph: tasks for adventurer 1 || tasks for adventurer 2 || tasks for adventurer 3'.
+      </p>
+      <br>
+      <p>LPT Algorithm<br>
+        Graph with baked fish:
+        1, 2 || 3, 4 || 6, 5, 7<br>
+        Graph with berries:
+        5,6 || 4, 7 || 1, 3, 2<br>
+        Graph with baked potatoes:
+        1, 4, 5 || 3, 2, 7 || 6<br>
+        <br>
+        HLFET Algorithm<br>
+        Graph with baked fish:
+        3, 4, 2 || 1,7 || 6, 5<br>
+        Graph with berries:
+        4, 7, 6 || 5 || 3, 1, 2<br>
+        Graph with baked potatoes:
+        3, 4, 5 || 1, 2, 7 || 6<br>
+      </p>
+    </div>
+  </div>
+
   <div class="btn-container">
     <button class="btn-back" @click="goToHome"><h2>Back to Home</h2></button>
   </div>
@@ -25,10 +53,14 @@ export default {
     goToHome() {
       this.$router.push({ name: 'Home' });
     },
+    toggleContent() {
+      this.isContentVisible = !this.isContentVisible;
+    }
   },
   data() {
     return {
-      gameUrl: '/gdevelop-game/index.html'
+      gameUrl: '/gdevelop-game/index.html',
+      isContentVisible: false
     };
   }
 };
@@ -41,4 +73,5 @@ export default {
   margin-right: 0;
   align-content: center;
 }
+
 </style>
